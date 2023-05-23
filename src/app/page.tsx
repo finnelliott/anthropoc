d776744c1cfe204a7d544e58c900275fe1f2dfe3
finnelliott <finn@finnelliott.com>
2023-05-23 15:43:51 +0100

    --

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ export default function Page() {
       <h1>anthropoc.com is for sale</h1>
       <p>I purchased anthropoc.com back in 2020 because I thought Anthropoc was a great brandable word with some strong potential for attached meaning.</p>
       <p><a href="https://www.anthropic.com">Anthropic</a> chose a very similar word to brand when founding their incredible AI business in 2021.</p>
-      <p>I believe this really limits the potential of Anthropoc as a brand name for the forseeable future. As a result, I'm letting anthropoc.com go.</p>
-      <p>If you're interested in purchasing anthropoc.com, please email me at <a href="mailto:finn@finnelliott.com">finn@finnelliott.com</a>.</p>
+      <p>I believe this really limits the potential of Anthropoc as a brand name for the forseeable future. As a result, I&apos;m letting anthropoc.com go.</p>
+      <p>If you&apos;re interested in purchasing anthropoc.com, please email me at <a href="mailto:finn@finnelliott.com">finn@finnelliott.com</a>.</p>
     </main>
   )
 }
